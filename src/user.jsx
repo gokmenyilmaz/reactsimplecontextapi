0e4c0f4edcd90fe339151a6cf03708f022d13053
gokmen.yilmaz <gokmen1977@gmail.com>
2019-12-20 01:04:40 +0300

    uu

--- src/user.jsx
+++ src/user.jsx
@@ -7,7 +7,7 @@ const Hello = ({name}) => <div>Hello {name}</div>
 export default class User extends React.Component {
   i=0;
 
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
 
   constructor(props) {
     super(props);
@@ -19,7 +19,11 @@ export default class User extends React.Component {
       <div>
         <Hello/>
         <h1>Kullanıcı</h1>
-        <h1>{this.context}</h1>
+        {/* <h1>{this.context}</h1> */}
+
+        <ThemeContext.Consumer>
+        {value => <h2>ddeger: {value}</h2>}
+      </ThemeContext.Consumer>
 
      
 
